42b331f9d0eafcd1ec27dfb550881bd4896ca3ec
juliannecc <julianneclave@gmail.com>
2023-04-15 00:57:09 +0800

    edit index

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+async function getMD(startPath, filter) {
+    if (!fs.existsSync(startPath)) {
+        core.info(`no dir ${startPath}`)
+        return;
+    }
+
+    var files = fs.readdirSync(startPath);
+    for (var i = 0; i < files.length; i++) {
+        var filename = path.join(startPath, files[i]);
+        var stat = fs.lstatSync(filename);
+        if (stat.isDirectory()) {
+            getMD(filename, filter); 
+        } else if (filename.endsWith(filter)) {
+            getMissingAlt(filename);
+        };
+    };
+};
+
 (
     async () => {
         try {
+            getMD('.', '.md')
             core.notice('hi')
-
         } catch (error) {
             core.setFailed(error.message);
         }
