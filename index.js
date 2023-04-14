163cce66d8d0c41e41ebfa885f959181f919f1d0
juliannecc <julianneclave@gmail.com>
2023-04-15 00:51:41 +0800

    edit index

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const core = require('@actions/core')
-const github = require('@actions/github')
+const core = require('@actions/core');
+const github = require('@actions/github');
 
 (
     async () => {
         try {
-            core.notice('Action');
-        } catch(error){
+            core.notice('hi')
+
+        } catch (error) {
             core.setFailed(error.message);
         }
     }
