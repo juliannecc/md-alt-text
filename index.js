8158c12829c8dfd925363028e40270ee08342d9a
juliannecc <julianneclave@gmail.com>
2023-04-15 01:34:30 +0800

    edit index

--- index.js
+++ index.js
@@ -33,7 +33,8 @@ async function getMissingAlt(filePath){
     });
     rl.on('line', (line) => {
         if (regex1.test(line)){
-            core.info(`line: ${line.match( /\((https?:\/\/)?(www\.)?[-a-zA-Z0-9@:%._\+~#=/]*\.(gif|jpg|jpeg|tiff|png|svg|ico)/gi)}`);
+            const imageLink = line.match( /\(https?:\/\/)?(www\.)?[-a-zA-Z0-9@:%._\+~#=/]*\.(gif|jpg|jpeg|tiff|png|svg|ico)/gi)
+            core.info(`line: ${imageLink}`);
         }
     });
     rl.on('close', () => {
