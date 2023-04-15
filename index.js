74016b6fb3932f17c3e6d235a66e5aebb1f3b5a3
juliannecc <julianneclave@gmail.com>
2023-04-15 12:39:37 +0800

    edit index

--- index.js
+++ index.js
@@ -84,7 +84,6 @@ async function getImageText(imageLink) {
 (
     async () => {
         try {
-            core.warning('test')
             getMD('.', '.md')
         } catch (error) {
             core.setFailed(error.message);
