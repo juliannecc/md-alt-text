fb221414746455f4131224073976c6941831cb6c
juliannecc <julianneclave@gmail.com>
2023-04-15 13:22:20 +0800

    edit index

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ async function getImageText(imageLink) {
                 {"Content-Type": "application/json",
                 "Ocp-Apim-Subscription-Key": `${AZURE_KEY}`}
             });
-        core.info(response[0]);
+        core.info(JSON.stringify(response.data)[0]);
         return;
     } catch (error) {
         core.warning(`Failed to get caption for image with link ${imageLink}`);
