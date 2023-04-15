d0246f47348749e446df7fbad0ceeaf4e401c1f0
juliannecc <julianneclave@gmail.com>
2023-04-15 14:00:45 +0800

    edit index

--- index.js
+++ index.js
@@ -59,8 +59,8 @@ async function getImageText(imageLink) {
                 {"Content-Type": "application/json",
                 "Ocp-Apim-Subscription-Key": `${AZURE_KEY}`}
             });
-        // core.info(JSON.stringify(response.data)[0]);
-        core.info(JSON.stringify(response.data['captionResult']['text']))
+        const result = JSON.stringify(response.data['captionResult']['text']);
+        core.info(result);
         return;
     } catch (error) {
         core.warning(`Failed to get caption for image with link ${imageLink}`);
