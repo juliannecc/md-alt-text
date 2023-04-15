2b86fb80a327f1cc6695f62647cd9fd4a0d3c103
juliannecc <julianneclave@gmail.com>
2023-04-16 01:08:54 +0800

    fix image link return result

--- index.js
+++ index.js
@@ -111,8 +111,8 @@ async function getImageText(imageLink) {
                 "Ocp-Apim-Subscription-Key": `${AZURE_KEY}`}
             });
         const result = JSON.stringify(response.data['captionResult']['text']);
-        core.info(result);
-        return;
+        // core.info(result);
+        return result;
     } catch (error) {
         core.warning(`Failed to get caption for image with link ${imageLink}`);
         core.warning(error);
