9dc2b55497f979032f743313bff0035424ac8fad
juliannecc <julianneclave@gmail.com>
2023-04-15 13:59:15 +0800

    edit index

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ async function getImageText(imageLink) {
                 "Ocp-Apim-Subscription-Key": `${AZURE_KEY}`}
             });
         // core.info(JSON.stringify(response.data)[0]);
-        core.info(JSON.stringify(response.data['captionResult']))
+        core.info(JSON.stringify(response.data['captionResult']['text']))
         return;
     } catch (error) {
         core.warning(`Failed to get caption for image with link ${imageLink}`);
