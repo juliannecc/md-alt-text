a8a16c8ee0788302b6ecc43c2967a0ac1d05ea02
juliannecc <julianneclave@gmail.com>
2023-04-15 18:45:18 +0800

    edit index

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ async function getMissingAlt(filePath){
             const owner = core.getInput('owner');
             const token = core.getInput('token');
             const repo = core.getInput('repo');
-            const branch = core.getInput('branch');
+            const branch = core.getInput('branch').toString().match(/\/(?:.(?!\/))+$/gim);
             core.info(`branch: ${branch}`);
                 
             if(imageLink.toString().startsWith('http')){
@@ -58,16 +58,16 @@ async function getMissingAlt(filePath){
                 }
                 var newImageLink = imageLink.toString().replace(imageLink, imageLink.toString().match(/\/(?:.(?!\/))+$/gim));
                 // var newLink = 'https://raw.githubusercontent.com/' + owner + '/' + repo + '/' + newPath + newImageLink;
-                var newLink = `https://raw.githubusercontent.com/${owner}/${repo}/${branch}/${newPath}${newImageLink}?token=${token}`
+                var newLink = `https://raw.githubusercontent.com/${owner}/${repo}${branch}/${newPath}${newImageLink}?token=${token}`
                 getImageText(newLink);
             } else if(imageLink.toString().startsWith('./')){
                 var cleanLink = imageLink.toString().replace('./','');
                 // var newLink = 'https://raw.githubusercontent.com/' + owner + '/' + repo + '/' + cleanLink;
-                var newLink = `https://raw.githubusercontent.com/${owner}/${repo}/${branch}/${cleanLink}?token=${token}`
+                var newLink = `https://raw.githubusercontent.com/${owner}/${repo}${branch}/${cleanLink}?token=${token}`
                 getImageText(newLink);
             } else {
                 // var newLink = 'https://github.com/' + owner + '/' + repo + '/' + imageLink;
-                var newLink = `https://raw.githubusercontent.com/${owner}/${repo}/${branch}/${imageLink}?token=${token}`
+                var newLink = `https://raw.githubusercontent.com/${owner}/${repo}${branch}/${imageLink}?token=${token}`
                 getImageText(newLink);
             }
 
